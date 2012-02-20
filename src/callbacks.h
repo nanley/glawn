61e2330f9f38e124c5478fe2caee32885fdeb909
Nanley Chery <nanleychery@gmail.com>
2012-02-19 22:35:53 -0500

    updated copyrights and readme link

--- src/callbacks.h
+++ src/callbacks.h
@@ -1,7 +1,7 @@
 /*
     This file is part of Glawn.
 
-    Copyright (C) 2010-2011  Nanley Chery <nanleychery@gmail.com>
+    Copyright (C) 2010-2012  Nanley Chery <nanleychery@gmail.com>
 
     Glawn is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
